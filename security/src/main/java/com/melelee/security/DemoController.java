8280ace0701b18f3ad63a14ad557bb3fdcf98d63
menglili <melelee@qq.com>
2023-05-25 23:26:57 +0800

    security

--- security/src/main/java/com/melelee/security/DemoController.java
+++ security/src/main/java/com/melelee/security/DemoController.java
@@ -1,12 +1,45 @@
 package com.melelee.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class DemoController {
     @GetMapping("/get")
-    public String get(){
+    public String get() {
         return "get";
+    }
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    public static final Map<String, String> MAP = new HashMap<>();
+
+    @PostMapping("/demo/login")
+    public String get(@RequestBody Map<String, String> map) {
+
+        Authentication request = new UsernamePasswordAuthenticationToken(map.get("username"), map.get("password"));
+        Authentication result = authenticationManager.authenticate(request);
+        if (result == null) {
+            return "fail";
+        }
+        Object principal = result.getPrincipal();
+        User user = (User) principal;
+
+        String token = UUID.randomUUID().toString();
+
+        MAP.put(user.getUsername(), token);
+        return token;
     }
 }
--- security/src/main/java/com/melelee/security/SecurityConfig.java
+++ security/src/main/java/com/melelee/security/SecurityConfig.java
@@ -2,17 +2,44 @@ package com.melelee.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig
-//        extends WebSecurityConfigurerAdapter
-{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    //todo 跟下面的区别
+//    @Override
+//    protected AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
+//    }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/demo/login")
+                .anonymous()
+                .anyRequest()
+                .authenticated();
+    }
 }
