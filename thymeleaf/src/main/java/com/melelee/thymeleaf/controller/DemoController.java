95e7f0c85ba33fa7fa87e6ebec137335fbe0b598
menglili <melelee@qq.com>
2023-10-25 09:11:57 +0800

    thymeleaf提交

---
+++ thymeleaf/src/main/java/com/melelee/thymeleaf/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.melelee.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping(value = "/demo")
+    public String demo(Model model) {
+        model.addAttribute("title", "后台返回的标题");
+        return "demo";
+    }
+}
