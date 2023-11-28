29341b9d75c91441ca726f494412c9d4f41de167
menglili <melelee@qq.com>
2023-11-28 20:16:38 +0800

    controller

--- knife4j/swagger/src/main/java/com/melelee/swagger/SwaggerApplication.java
+++ knife4j/swagger/src/main/java/com/melelee/swagger/SwaggerApplication.java
@@ -2,10 +2,14 @@ package com.melelee.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * Hello world!
  */
+
+@EnableSwagger2WebMvc
+
 @SpringBootApplication
 public class SwaggerApplication {
     public static void main(String[] args) {
