0397aed0acac057eeb0ac629e823f0147e28feec
menglili <melelee@qq.com>
2023-11-29 21:47:23 +0800

    spring

---
+++ spring/src/main/java/com/melelee/spring/beans/BeansDemo.java
@@ -0,0 +1,17 @@
+package com.melelee.spring.beans;
+
+import org.springframework.context.ApplicationContext;
+
+public class BeansDemo {
+    public static void main(String[] args) {
+//        BeanDefinition
+//        BeanDefinitionReader
+
+//        BeanPostProcessor
+//        BeanFactoryPostProcessor
+
+//BeanFactory
+//        ApplicationContext
+    }
+
+}
